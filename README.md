54ed1dd68c49d16fc3c5ce555df0ba3be1a75b46
dullmatian <someone_6262@yahoo.co.jp>
2022-07-28 15:55:47 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# flask-tweepy-auth 
+ 
+### Features
+get personal access token and access token secret(consumer keys) using tweepy and flask
+ 
+### Requirement
+
+* tweepy==3.9.0
+* Flask==1.1.2
+* .env file
+
+### Installation
+```bash
+pip install tweepy
+pip install Flask
+```
+```bash
+git pull 
+```
+```bash
+echo -e API_KEY=xxxxxxxx\\n\
+    API_KEY_SECRET=xxxxxxxx > service/.env
+```
+ 
+### Usage
+```bash
+cd flask-tweepy-auth
+python service/main.py
+```
+ 
+### Note
+you need to set `http://127.0.0.1:5000/callback` to callback url, which is in twitter developer portal 
---
+++ service/main.py
@@ -0,0 +1,32 @@
+import flask
+from flask import redirect, request
+import tweepy
+import os
+from dotenv import load_dotenv
+load_dotenv()
+ 
+api_key = os.getenv('API_KEY')
+api_secret = os.getenv('API_KEY_SECRET')
+ 
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+    auth = tweepy.OAuthHandler(api_key, api_secret)
+    return redirect(auth.get_authorization_url())
+
+@app.route('/callback', methods=['GET', 'POST'])
+def callback():
+    args = request.args
+    oauth_token = args['oauth_token']
+    oauth_verifier = args['oauth_verifier']
+    auth = tweepy.OAuthHandler(api_key, api_secret)
+    auth.request_token = {'oauth_token': oauth_token, 'oauth_token_secret': oauth_verifier}
+    auth.get_access_token(oauth_verifier)
+
+    user_tokens = f"access-token={auth.access_token}<br>access-token-secret={auth.access_token_secret}"
+    return user_tokens
+
+if(__name__ == "__main__"):
+    app.run(port=5000)
